39b5c1efadc6bf15195dc9d6446a62adde4c80b0
jameelag1995 <agbariajameel@gmail.com>
2023-09-27 15:35:18 +0300

    added ex 7 in bonus

--- bonus.js
+++ bonus.js
@@ -122,6 +122,90 @@
 // );
 
 /* -------------------------------------------------------------------------- */
+/*                                    Ex 7                                    */
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+/*                             filter  for arrays                             */
+/* -------------------------------------------------------------------------- */
+function myFilter(originalArray, callback) {
+    const newArr = [];
+    for (const element of originalArray) {
+        if (callback(element)) {
+            newArr.push(element);
+        }
+    }
+    return newArr;
+}
+/* ---------------------------------- Test ---------------------------------- */
+const nums = [3, 4, 6, 7];
+const bigger = (n) => {
+    return n > 5;
+};
+console.log("original array:", nums);
+console.log("array filter:", myFilter(nums, bigger));
+/* -------------------------------------------------------------------------- */
+/*                               map for arrays                               */
+/* -------------------------------------------------------------------------- */
+function myMap(originalArray, callback) {
+    const newArr = [];
+    for (const element of originalArray) {
+        newArr.push(callback(element));
+    }
+    return newArr;
+}
+/* ---------------------------------- Test ---------------------------------- */
+const square = (n) => {
+    return n * n;
+};
+console.log("*****");
+console.log("original array:", nums);
+console.log("array map:", myMap(nums, square));
+
+/* -------------------------------------------------------------------------- */
+/*                               filter objects                               */
+/* -------------------------------------------------------------------------- */
+function myObjFilter(obj, callback) {
+    const newObj = {};
+    for (const key in obj) {
+        if (callback(obj[key])) {
+            newObj[key] = obj[key];
+        }
+    }
+    return newObj;
+}
+/* ---------------------------------- Test ---------------------------------- */
+const myobj = {
+    name: "jameel",
+    age: 28,
+    address: "52street",
+};
+const test = (prop) => {
+    return prop > 30 || prop.length > 7;
+};
+console.log("*****");
+console.log("original object:", myobj);
+console.log("object filter:", myObjFilter(myobj, test));
+
+/* -------------------------------------------------------------------------- */
+/*                                 map for objects                                */
+/* -------------------------------------------------------------------------- */
+function myObjMap(obj, callback) {
+    const newObj = {};
+    for (const key in obj) {
+        newObj[key] = callback(obj[key]);
+    }
+    return newObj;
+}
+/* ---------------------------------- Test ---------------------------------- */
+const test2 = (prop) => {
+    return typeof prop;
+};
+console.log("*****");
+console.log("original object:", myobj);
+console.log("object Map:", myObjMap(myobj, test2));
+
+
+/* -------------------------------------------------------------------------- */
 /*                    Ex Find the Perimeter of a Rectangle                    */
 /* -------------------------------------------------------------------------- */
 // function rectanglePerimeter(length, width) {
